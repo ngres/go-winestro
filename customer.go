6d27196b2db9a0610d39dcb66b8b57bc0733c131
Nicolas Gres <nicolas@gres.io>
2026-04-27 15:58:19 +0200

    add omit empty

--- customer.go
+++ customer.go
@@ -10,30 +10,30 @@ import (
 type Customer struct {
 	ID               int32   `xml:"adr_id" json:"id"`
 	Number           string  `xml:"adr_nr" json:"number"`
-	FirstName        string  `xml:"adr_vorname" json:"first_name"`
-	LastName         string  `xml:"adr_nachname" json:"last_name"`
-	Company          string  `xml:"adr_firma" json:"company"`
-	ZIP              string  `xml:"adr_plz" json:"zip"`
-	City             string  `xml:"adr_ort" json:"city"`
-	WWW              string  `xml:"adr_www" json:"www"`
-	Email            string  `xml:"adr_email" json:"email"`
-	Street           string  `xml:"adr_str" json:"street"`
-	HouseNum         string  `xml:"adr_str_nr" json:"house_number"`
-	Country          string  `xml:"adr_land" json:"country"`
-	Landline         string  `xml:"adr_festnetz" json:"landline"`
-	Mobile           string  `xml:"adr_mobil" json:"mobile"`
-	Fax              string  `xml:"adr_fax" json:"fax"`
-	Note1            string  `xml:"adr_note1" json:"note1"`
-	Note2            string  `xml:"adr_note2" json:"note2"`
-	Note3            string  `xml:"adr_note3" json:"note3"`
-	Note4            string  `xml:"adr_note4" json:"note4"`
-	Discount         float64 `xml:"adr_rabatt" json:"discount"`
-	PriceCategory    int     `xml:"adr_id_preiskategorie" json:"price_category"`
-	NewsletterActive bool    `xml:"adr_newsletter_aktiv" json:"newsletter_active"`
-	TaxType          int     `xml:"adr_kunden_mwst" json:"tax_type"`
-	Salutation       string  `xml:"adr_anrede" json:"salutation"`
-	SalutationType   int     `xml:"adr_anredenart" json:"salutation_type"`
-	PaymentType      int     `xml:"adr_id_zahlungsart" json:"payment_type"`
+	FirstName        string  `xml:"adr_vorname" json:"first_name,omitempty"`
+	LastName         string  `xml:"adr_nachname" json:"last_name,omitempty"`
+	Company          string  `xml:"adr_firma" json:"company,omitempty"`
+	ZIP              string  `xml:"adr_plz" json:"zip,omitempty"`
+	City             string  `xml:"adr_ort" json:"city,omitempty"`
+	WWW              string  `xml:"adr_www" json:"www,omitempty"`
+	Email            string  `xml:"adr_email" json:"email,omitempty"`
+	Street           string  `xml:"adr_str" json:"street,omitempty"`
+	HouseNum         string  `xml:"adr_str_nr" json:"house_number,omitempty"`
+	Country          string  `xml:"adr_land" json:"country,omitempty"`
+	Landline         string  `xml:"adr_festnetz" json:"landline,omitempty"`
+	Mobile           string  `xml:"adr_mobil" json:"mobile,omitempty"`
+	Fax              string  `xml:"adr_fax" json:"fax,omitempty"`
+	Note1            string  `xml:"adr_note1" json:"note1,omitempty"`
+	Note2            string  `xml:"adr_note2" json:"note2,omitempty"`
+	Note3            string  `xml:"adr_note3" json:"note3,omitempty"`
+	Note4            string  `xml:"adr_note4" json:"note4,omitempty"`
+	Discount         float64 `xml:"adr_rabatt" json:"discount,omitempty"`
+	PriceCategory    int     `xml:"adr_id_preiskategorie" json:"price_category,omitempty"`
+	NewsletterActive bool    `xml:"adr_newsletter_aktiv" json:"newsletter_active,omitempty"`
+	TaxType          int     `xml:"adr_kunden_mwst" json:"tax_type,omitempty"`
+	Salutation       string  `xml:"adr_anrede" json:"salutation,omitempty"`
+	SalutationType   int     `xml:"adr_anredenart" json:"salutation_type,omitempty"`
+	PaymentType      int     `xml:"adr_id_zahlungsart" json:"payment_type,omitempty"`
 }
 
 func (c Customer) FullName() string {
--- product.go
+++ product.go
@@ -30,12 +30,12 @@ type BundleItem struct {
 
 type ProductNuance struct {
 	Name  string `xml:"artikel_nuancen_item_name" json:"name"`
-	Image string `xml:"artikel_nuancen_item_image" json:"image"`
+	Image string `xml:"artikel_nuancen_item_image" json:"image,omitempty"`
 }
 
 type ProductAward struct {
 	Name  string `xml:"artikel_auszeichnungen_name" json:"name"`
-	Image string `xml:"artikel_auszeichnungen_image" json:"image"`
+	Image string `xml:"artikel_auszeichnungen_image" json:"image,omitempty"`
 }
 
 type ProductFoodPairing struct {
@@ -45,84 +45,84 @@ type ProductFoodPairing struct {
 type Product struct {
 	SKU                string               `xml:"artikel_nr" json:"sku"`
 	Name               string               `xml:"artikel_name" json:"name"`
-	Description        string               `xml:"artikel_beschreibung" json:"description"`
-	Vintage            string               `xml:"artikel_jahrgang" json:"vintage"`
-	Variety            string               `xml:"artikel_sorte" json:"variety"`
-	Quality            string               `xml:"artikel_qualitaet" json:"quality"`
-	Taste              string               `xml:"artikel_geschmack" json:"taste"`
-	Sugar              string               `xml:"artikel_zucker" json:"sugar"`
-	Alcohol            string               `xml:"artikel_alkohol" json:"alcohol"`
-	Acid               string               `xml:"artikel_saeure" json:"acid"`
-	VolumeLiter        float32              `xml:"artikel_liter" json:"volume_liter"`
-	WeightKg           float32              `xml:"artikel_gewicht" json:"weight"`
-	Sulfites           bool                 `xml:"artikel_sulfite" json:"sulfites"`
-	Image              string               `xml:"artikel_bild" json:"image"`
-	ImageLarge         string               `xml:"artikel_bild_big" json:"image_large"`
-	Image2             string               `xml:"artikel_bild_2" json:"image_2"`
-	Image2Large        string               `xml:"artikel_bild_big_2" json:"image_2_large"`
-	Image3             string               `xml:"artikel_bild_3" json:"image_3"`
-	Image3Large        string               `xml:"artikel_bild_big_3" json:"image_3_large"`
-	Image4             string               `xml:"artikel_bild_4" json:"image_4"`
-	Image4Large        string               `xml:"artikel_bild_big_4" json:"image_4_large"`
-	ShippingQuantity   int                  `xml:"artikel_versandmenge" json:"shipping_quantity"`
-	BundleItems        []BundleItem         `xml:"artikel_sort_items>artikel_sort_item" json:"bundle_items"`
-	Price              float32              `xml:"artikel_preis" json:"price"`
-	PriceLiter         float32              `xml:"artikel_literpreis" json:"price_liter"`
-	VAT                float32              `xml:"artikel_mwst" json:"vat"`
-	Calories           string               `xml:"artikel_brennwert" json:"calories"`
-	Protein            string               `xml:"artikel_eiweiss" json:"protein"`
-	FreeShipping       bool                 `xml:"artikel_versandfrei" json:"free_shipping"`
-	HidePriceLiter     bool                 `xml:"artikel_keinliterpreis" json:"hide_price_liter"`
-	FillWeightGram     int                  `xml:"artikel_fuellgewicht" json:"fill_weight_gram"`
-	PriceKg            float32              `xml:"artikel_kilopreis" json:"price_kg"`
-	OutOfStock         bool                 `xml:"artikel_ausgetrunken" json:"out_of_stock"`
-	APNR               string               `xml:"artikel_apnr" json:"apnr"`
-	Vineyard           string               `xml:"artikel_lage" json:"vineyardn"`
-	Expertise          string               `xml:"artikel_expertise" json:"expertise"`
-	TypeName           string               `xml:"artikel_typ" json:"type_name"`
-	TypeID             int                  `xml:"artikel_typ_id" json:"type_id"`
-	Color              string               `xml:"artikel_farbe" json:"color"`
-	DrinkTemperature   string               `xml:"artikel_trinktemperatur" json:"drink_temperature"`
-	StorageTemperature string               `xml:"artikel_lagertemperatur" json:"storage_temperature"`
-	Elaboration        string               `xml:"artikel_ausbau" json:"elaboration"`
-	Soil               string               `xml:"artikel_boden" json:"soil"`
-	StorableYears      string               `xml:"artikel_lagerfaehigkeit" json:"storable_years"`
-	VideoURL           string               `xml:"artikel_video" json:"video_url"`
-	Country            string               `xml:"artikel_land" json:"country"`
-	Region             string               `xml:"artikel_region" json:"region"`
-	Appellation        string               `xml:"artikel_anbaugebiet" json:"appellation"`
-	StockWarningLevel  int                  `xml:"artikel_bestand_warnung_ab" json:"stock_warning_level"`
-	ProducerID         int                  `xml:"artikel_erzeuger" json:"producer_id"`
-	ProducerName       string               `xml:"artikel_erzeuger_name" json:"producer_name"`
-	ProducerNumber     string               `xml:"artikel_erzeuger_nr" json:"producer_number"`
-	Category           string               `xml:"artikel_kategorie" json:"category"`
-	PackagingID        int                  `xml:"artikel_verpackung" json:"packaging_id"`
-	PackagingName      string               `xml:"artikel_verpackung_bezeichnung	" json:"packaging_name"`
-	PackagingQuantity  int                  `xml:"artikel_verpackung_inhalt" json:"packaging_quantity"`
-	EAN13              string               `xml:"artikel_ean13" json:"ean13"`
-	EAN13Packaging     string               `xml:"artikel_ean13_kiste" json:"ean13_packaging"`
-	Ingredients        string               `xml:"artikel_zutaten" json:"ingredients"`
-	BestByDate         string               `xml:"artikel_mhd" json:"best_by_date"`
-	Fat                string               `xml:"artikel_fett" json:"fat"`
-	UnsaturatedFat     string               `xml:"artikel_fetts" json:"unsaturated_fat"`
-	Carbohydrates      string               `xml:"artikel_kohlenhydrate" json:"carbohydrates"`
-	Salt               string               `xml:"artikel_salz" json:"salt"`
-	Fibre              string               `xml:"artikel_ballast" json:"fibre"`
-	Vitamins           string               `xml:"artikel_vitamine" json:"vitamins"`
-	SulfuricAcids      string               `xml:"artikel_gesamt_schwefelsaeure" json:"sulfuric_acids"`
-	FreeSulfuricAcids  string               `xml:"artikel_frei_schwefelsaeure" json:"free_sulfuric_acids"`
-	Histamine          string               `xml:"artikel_histamin" json:"histamine"`
-	Glycerin           string               `xml:"artikel_glycerin" json:"glycerin"`
-	ELabelText         string               `xml:"artikel_labeltext" json:"elabel_text"`
-	ELabelURL          string               `xml:"artikel_labellink" json:"elabel_url"`
-	ProductGroups      []string             `xml:"artikel_warengruppen>warengruppe" json:"product_groups"`
-	Stock              float32              `xml:"artikel_bestand" json:"stock"`
-	CompanyStock       float32              `xml:"artikel_bestand_firmenverbund" json:"company_stock"`
-	WebshopStock       float32              `xml:"artikel_bestand_webshop" json:"webshop_stock"`
-	LastModifiedDate   timestamp            `xml:"artikel_last_modified" json:"last_modified_date"`
-	NuanceItems        []ProductNuance      `xml:"artikel_nuancen_items>artikel_nuancen_item" json:"nuance_items"`
-	AwardItems         []ProductAward       `xml:"artikel_auszeichnungen>artikel_auszeichnungen_item" json:"award_items"`
-	FoodPairingItems   []ProductFoodPairing `xml:"artikel_speisen_items>artikel_speisen_item" json:"food_pairing_items"`
+	Description        string               `xml:"artikel_beschreibung" json:"description,omitempty"`
+	Vintage            string               `xml:"artikel_jahrgang" json:"vintage,omitempty"`
+	Variety            string               `xml:"artikel_sorte" json:"variety,omitempty"`
+	Quality            string               `xml:"artikel_qualitaet" json:"quality,omitempty"`
+	Taste              string               `xml:"artikel_geschmack" json:"taste,omitempty"`
+	Sugar              string               `xml:"artikel_zucker" json:"sugar,omitempty"`
+	Alcohol            string               `xml:"artikel_alkohol" json:"alcohol,omitempty"`
+	Acid               string               `xml:"artikel_saeure" json:"acid,omitempty"`
+	VolumeLiter        float32              `xml:"artikel_liter" json:"volume_liter,omitempty"`
+	WeightKg           float32              `xml:"artikel_gewicht" json:"weight,omitempty"`
+	Sulfites           bool                 `xml:"artikel_sulfite" json:"sulfites,omitempty"`
+	Image              string               `xml:"artikel_bild" json:"image,omitempty"`
+	ImageLarge         string               `xml:"artikel_bild_big" json:"image_large,omitempty"`
+	Image2             string               `xml:"artikel_bild_2" json:"image_2,omitempty"`
+	Image2Large        string               `xml:"artikel_bild_big_2" json:"image_2_large,omitempty"`
+	Image3             string               `xml:"artikel_bild_3" json:"image_3,omitempty"`
+	Image3Large        string               `xml:"artikel_bild_big_3" json:"image_3_large,omitempty"`
+	Image4             string               `xml:"artikel_bild_4" json:"image_4,omitempty"`
+	Image4Large        string               `xml:"artikel_bild_big_4" json:"image_4_large,omitempty"`
+	ShippingQuantity   int                  `xml:"artikel_versandmenge" json:"shipping_quantity,omitempty"`
+	BundleItems        []BundleItem         `xml:"artikel_sort_items>artikel_sort_item" json:"bundle_items,omitempty"`
+	Price              float32              `xml:"artikel_preis" json:"price,omitempty"`
+	PriceLiter         float32              `xml:"artikel_literpreis" json:"price_liter,omitempty"`
+	VAT                float32              `xml:"artikel_mwst" json:"vat,omitempty"`
+	Calories           string               `xml:"artikel_brennwert" json:"calories,omitempty"`
+	Protein            string               `xml:"artikel_eiweiss" json:"protein,omitempty"`
+	FreeShipping       bool                 `xml:"artikel_versandfrei" json:"free_shipping,omitempty"`
+	HidePriceLiter     bool                 `xml:"artikel_keinliterpreis" json:"hide_price_liter,omitempty"`
+	FillWeightGram     int                  `xml:"artikel_fuellgewicht" json:"fill_weight_gram,omitempty"`
+	PriceKg            float32              `xml:"artikel_kilopreis" json:"price_kg,omitempty"`
+	OutOfStock         bool                 `xml:"artikel_ausgetrunken" json:"out_of_stock,omitempty"`
+	APNR               string               `xml:"artikel_apnr" json:"apnr,omitempty"`
+	Vineyard           string               `xml:"artikel_lage" json:"vineyard,omitempty"`
+	Expertise          string               `xml:"artikel_expertise" json:"expertise,omitempty"`
+	TypeName           string               `xml:"artikel_typ" json:"type_name,omitempty"`
+	TypeID             int                  `xml:"artikel_typ_id" json:"type_id,omitempty"`
+	Color              string               `xml:"artikel_farbe" json:"color,omitempty"`
+	DrinkTemperature   string               `xml:"artikel_trinktemperatur" json:"drink_temperature,omitempty"`
+	StorageTemperature string               `xml:"artikel_lagertemperatur" json:"storage_temperature,omitempty"`
+	Elaboration        string               `xml:"artikel_ausbau" json:"elaboration,omitempty"`
+	Soil               string               `xml:"artikel_boden" json:"soil,omitempty"`
+	StorableYears      string               `xml:"artikel_lagerfaehigkeit" json:"storable_years,omitempty"`
+	VideoURL           string               `xml:"artikel_video" json:"video_url,omitempty"`
+	Country            string               `xml:"artikel_land" json:"country,omitempty"`
+	Region             string               `xml:"artikel_region" json:"region,omitempty"`
+	Appellation        string               `xml:"artikel_anbaugebiet" json:"appellation,omitempty"`
+	StockWarningLevel  int                  `xml:"artikel_bestand_warnung_ab" json:"stock_warning_level,omitempty"`
+	ProducerID         int                  `xml:"artikel_erzeuger" json:"producer_id,omitempty"`
+	ProducerName       string               `xml:"artikel_erzeuger_name" json:"producer_name,omitempty"`
+	ProducerNumber     string               `xml:"artikel_erzeuger_nr" json:"producer_number,omitempty"`
+	Category           string               `xml:"artikel_kategorie" json:"category,omitempty"`
+	PackagingID        int                  `xml:"artikel_verpackung" json:"packaging_id,omitempty"`
+	PackagingName      string               `xml:"artikel_verpackung_bezeichnung	" json:"packaging_name,omitempty"`
+	PackagingQuantity  int                  `xml:"artikel_verpackung_inhalt" json:"packaging_quantity,omitempty"`
+	EAN13              string               `xml:"artikel_ean13" json:"ean13,omitempty"`
+	EAN13Packaging     string               `xml:"artikel_ean13_kiste" json:"ean13_packaging,omitempty"`
+	Ingredients        string               `xml:"artikel_zutaten" json:"ingredients,omitempty"`
+	BestByDate         string               `xml:"artikel_mhd" json:"best_by_date,omitempty"`
+	Fat                string               `xml:"artikel_fett" json:"fat,omitempty"`
+	UnsaturatedFat     string               `xml:"artikel_fetts" json:"unsaturated_fat,omitempty"`
+	Carbohydrates      string               `xml:"artikel_kohlenhydrate" json:"carbohydrates,omitempty"`
+	Salt               string               `xml:"artikel_salz" json:"salt,omitempty"`
+	Fibre              string               `xml:"artikel_ballast" json:"fibre,omitempty"`
+	Vitamins           string               `xml:"artikel_vitamine" json:"vitamins,omitempty"`
+	SulfuricAcids      string               `xml:"artikel_gesamt_schwefelsaeure" json:"sulfuric_acids,omitempty"`
+	FreeSulfuricAcids  string               `xml:"artikel_frei_schwefelsaeure" json:"free_sulfuric_acids,omitempty"`
+	Histamine          string               `xml:"artikel_histamin" json:"histamine,omitempty"`
+	Glycerin           string               `xml:"artikel_glycerin" json:"glycerin,omitempty"`
+	ELabelText         string               `xml:"artikel_labeltext" json:"elabel_text,omitempty"`
+	ELabelURL          string               `xml:"artikel_labellink" json:"elabel_url,omitempty"`
+	ProductGroups      []string             `xml:"artikel_warengruppen>warengruppe" json:"product_groups,omitempty"`
+	Stock              float32              `xml:"artikel_bestand" json:"stock,omitempty"`
+	CompanyStock       float32              `xml:"artikel_bestand_firmenverbund" json:"company_stock,omitempty"`
+	WebshopStock       float32              `xml:"artikel_bestand_webshop" json:"webshop_stock,omitempty"`
+	LastModifiedDate   timestamp            `xml:"artikel_last_modified" json:"last_modified_date,omitempty"`
+	NuanceItems        []ProductNuance      `xml:"artikel_nuancen_items>artikel_nuancen_item" json:"nuance_items,omitempty"`
+	AwardItems         []ProductAward       `xml:"artikel_auszeichnungen>artikel_auszeichnungen_item" json:"award_items,omitempty"`
+	FoodPairingItems   []ProductFoodPairing `xml:"artikel_speisen_items>artikel_speisen_item" json:"food_pairing_items,omitempty"`
 }
 
 func (p *Product) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
